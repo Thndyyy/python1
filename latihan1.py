ac94a2de478c4dc09127b0c0c85ec34f52de8dd3
Thondy Autarliandi <148066593+Thndyyy@users.noreply.github.com>
2023-10-22 20:18:16 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,4 @@
+nama = input("Masukkan nama Anda:Thondy Autarliandi ")
+umur = int(input("Masukkan umur Anda:19 tahun "))
+
+print("Halo, nama saya", nama, "dan saya berusia", umur, "tahun.")
---
+++ latihan3.py
@@ -0,0 +1,20 @@
+# Input nilai variabel
+a = input("Masukkan nilai a:8 ")
+b = input("Masukkan nilai b:10 ")
+
+# Konversi input ke tipe data integer
+a = int(a)
+b = int(b)
+
+# Cetak nilai variabel
+print("Variabel a =", a)
+print("Variabel b =", b)
+
+# Cetak nilai operasi kedua variabel dengan String Format
+print("Hasil penggabung {}&{} = {}".format(a, b, a + b))
+
+# Hasil penjumlahan
+print("Hasil penjumlahan {} + {} = {}".format(a, b, a + b))
+
+# Hasil pembagian
+print("Hasil pembagian {} / {} = {}".format(a, b, a / b))
